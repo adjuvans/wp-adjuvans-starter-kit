b3a558ae1091fd0f83c6a1b0986ca84537a72888
cdegourcy <cyrille@gourcy.net>
2022-03-11 17:11:08 +0100

    dev

--- cli/install-wordpress.sh
+++ cli/install-wordpress.sh
@@ -5,7 +5,10 @@ echo " "
 echo "---"
 echo "${blue}${bold}# WordPress Installer${normal}"
 
-wp core download
-wp config create --dbhost=${db_host} --dbname=${db_name} --dbuser=${db_user} --dbpass=${db_pass} --dbprefix=${db_prefix} --locale=${site_locale}
-
+#wp core download
+wp config create --dbhost=${db_host} --dbname=${db_name} --dbuser=${db_user} --dbprefix=${db_prefix} --locale=${site_locale} --dbpass=${db_pass} --extra-php <<PHP
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'WP_DEBUG_LOG', '../logs/'.date('Y-m-d').'_wp-errors.log' );
+PHP
+wp core install --url=${site_url} --title=${site_title} --admin_user=${admin_login} --admin_email=${admin_email}
 echo "---"
